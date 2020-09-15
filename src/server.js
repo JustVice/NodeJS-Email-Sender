1c8e1a68260e0b379e4242e35896fbc9975cc2de
JustVice <43510837+JustVice@users.noreply.github.com>
2020-09-14 21:00:06 -0600

    CORS allow all connections fix

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ app.set("PORT", process.env.PORT || 3000);
 app.use(express.urlencoded({extended: false})); // No complex files understanding like images.
 app.use(express.json()); // Server understands JSON.
 
+// Setting the application to response it's API to any domain on the web.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Routes.
 app.use(require("./routes/index.js"));
 
